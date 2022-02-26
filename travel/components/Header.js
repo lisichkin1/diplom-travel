2d9292bef4a7888e8498e0cd12377e368c3f75f5
Ivan <ivanlistop@gmail.com>
2022-02-26 16:45:49 +0300

    feat: add styles for icon

--- travel/components/Header.js
+++ travel/components/Header.js
@@ -3,9 +3,12 @@ import Image from 'next/image';
 import logoPic from '../image/travel.png'
 function Header() {
   return (
-    <header className='sticky top-0 z-50'>
+    <header className='sticky top-0 z-50 
+    grid grid-cols-3 
+    bg-white shadow-md
+     py-5 px-5 md:px-10'>
       {/* left */}
-      <div className='relative flex items-center h-20 cursor-pointer my-auto'>
+      <div className='relative flex items-center h-16 cursor-pointer my-auto'>
       <Image src={logoPic} 
       layout='fill' 
       objectFit="contain"
