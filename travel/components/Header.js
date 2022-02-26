b09b9375918f8bf8f4d78b49df1b6fb6e8d44cc6
Ivan <ivanlistop@gmail.com>
2022-02-26 18:25:10 +0300

    feat:add a search bar

--- travel/components/Header.js
+++ travel/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
 import logoPic from '../image/travel.png'
+import { SearchIcon } from '@heroicons/react/solid'
 function Header() {
   return (
     <header className='sticky top-0 z-50 
@@ -8,17 +9,26 @@ function Header() {
     bg-white shadow-md
      py-5 px-5 md:px-10'>
       {/* left */}
-      <div className='relative flex items-center h-16 cursor-pointer my-auto'>
+      <div className='relative flex items-center h-12 cursor-pointer my-auto'>
       <Image src={logoPic} 
-      layout='fill' 
+      width='80'
+      height='80' 
       objectFit="contain"
       objectPosition="left"
       />
       </div>
       {/* middle */}
 
-      <div>
-
+      <div className='flex 
+      items-center md:border-2 
+      rounded-full py-2
+      md:shadow-md'>
+        <input className='pl-5 flex-grow 
+        bg-transparent outline-none
+        text-base text-gray-600 placeholder-gray-400' 
+        type="text" placeholder='Начни свой поиск...'/>
+        <SearchIcon className='h-8 bg-blue-400 text-white 
+        p-2 rounded-full cursor-pointer hidden md:inline-flex md:mx-3'/>
       </div>
 
       {/* right */}
