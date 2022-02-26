98c14ff768931e7fdc3c980d3d3fb83651670b29
Ivan <ivanlistop@gmail.com>
2022-02-26 13:56:18 +0300

    feat: add a logo to the page

--- travel/components/Header.js
+++ travel/components/Header.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import Image from 'next/image';
+import logoPic from '../image/travel.png'
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className='sticky top-0 z-50'>
+      {/* left */}
+      <div className='relative flex items-center h-20 cursor-pointer my-auto'>
+      <Image src={logoPic} 
+      layout='fill' 
+      objectFit="contain"
+      objectPosition="left"
+      />
+      </div>
+      {/* middle */}
+
+      <div>
+
+      </div>
+
+      {/* right */}
+
+      <div>
+
+      </div>
+    </header>
+    
+  );
 }
 
 export default Header
