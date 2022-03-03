e61577aa983aaf0919c9d7a9de0071038f44b507
Ivan <ivanlistop@gmail.com>
2022-03-03 12:04:00 +0300

    feat:add slider in app

--- travel/pages/index.js
+++ travel/pages/index.js
@@ -3,11 +3,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header"
 import Banner from "../components/Banner"
+import SliderCard from"../components/SliderCard"
 import SityCard from"../components/SityCard"
 
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
-export default function Home({exploreData}){
+export default function Home({exploreData, sliderData}){
   return (
+    
     <div className="">
       <Head>
         <title>Create Next App</title>
@@ -19,7 +24,8 @@ export default function Home({exploreData}){
       <Banner/>
     <main className='max-w-7xl mx-auto px-8 sm:px-16'>
       <section className='pt-6'>
-      <h2 className='text-2xl sm:text-4xl text-black font-semibold pb-6'>
+      <h2 className='text-2xl sm:text-4xl text-black 
+      font-semibold pb-6'>
         Исследуйте популярные места
       </h2>
       {/* pull data from a server - API */}
@@ -37,6 +43,23 @@ export default function Home({exploreData}){
         ))}
       </div>
       </section>
+
+      
+      <section>
+        <h2 className=' text-2xl sm:text-4xl text-black 
+        font-semibold py-6'>Живите где угодно</h2>
+        
+        <div className='flex space-x-5 overflow-scroll'> 
+        {sliderData?.map(({img, title}) =>(
+          <SliderCard
+          key = {img}
+          img = {img}
+          title = {title}
+          />
+        ))}
+        
+        </div>
+      </section>
     </main>
     </div>
   )
@@ -47,10 +70,15 @@ export async function getStaticProps() {
   then(
     (res) => res.json()
     );
-
+  
+  const sliderData = await fetch("https://jsonkeeper.com/b/G0PD").
+  then(
+    (res) => res.json()
+     );
     return{
       props:{
         exploreData,
+        sliderData,
       },
     };
 }
