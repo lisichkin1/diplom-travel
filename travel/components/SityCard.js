8fa00c8156672b8ab60b27ececa2a939538de5d2
Ivan <ivanlistop@gmail.com>
2022-03-11 21:57:41 +0300

    fix: add other cities

--- travel/components/SityCard.js
+++ travel/components/SityCard.js
@@ -3,13 +3,13 @@ import Image from 'next/image'
 function SityCard({img, location, distance}) {
   return (
     <div className='flex items-center 
-    m-3 mt-5 space-x-5 rounded-xl 
+    m-3 mt-5 space-x-7 overflow-hidden rounded-xl 
     cursor-pointer hover:bg-gray-200
     hover:scale-105 transition transform 
     duration-300 ease-out'>
         {/* left */}
-        <div className='relative h-20 w-20'>
-            <Image src={img} layout='fill' className='rounded-2xl'/>
+        <div className='relative w-[90px] h-[90px] transform scale-150'>
+            <Image src={img} layout='fill' className=' absolute rounded-full'/>
         </div>
         {/* right */}
         <h2 className='text-gray-600'>{location}</h2>
