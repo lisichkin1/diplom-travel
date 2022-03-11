31b1b22c2b11871a890f589561c11cf6913de09f
Ivan <ivanlistop@gmail.com>
2022-03-12 02:06:59 +0300

    feat: add search for cities

--- travel/components/SityCard.js
+++ travel/components/SityCard.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 function SityCard({img, location, distance}) {
+
+  const router = useRouter();
+  const{startDate, endDate, quantityOfGuest} = router.query
   return (
-    <div className='flex items-center 
+    <div onClick={() => router.push(`search?location=${location}&startDate=2022-03-11T22%3A48%3A44.217Z&endDate=2022-03-11T22%3A48%3A44.217Z&quantityOfGuest=1`)} className='flex items-center 
     m-3 mt-5 space-x-7 overflow-hidden rounded-xl 
     cursor-pointer hover:bg-gray-200
     hover:scale-105 transition transform 
@@ -16,5 +20,7 @@ function SityCard({img, location, distance}) {
     </div>
   )
 }
+//http://localhost:3000/search?location=Саратов&startDate=2022-03-11T22%3A54%3A23.741Z&endDate=2022-03-11T22%3A54%3A23.741Z&quantityOfGuest=1
+//http://localhost:3000/search/search?location=Саратов&startDate=2022-03-11T22%3A48%3A44.217Z&endDate=2022-03-11T22%3A48%3A44.217Z&quantityOfGuest=1
 
 export default SityCard
